8ebd55ad01c3018f451da81cec00b3b216bfddf5
Glen <glen.watson@alumni.neumont.edu>
2012-07-23 17:05:44 -0400

    Implemented Generic SquareArrayExtentions in MapBuilder Found error with map generation, never generate nodes past head

--- View/Input/AI/Logic/MapModel/MapBuilder.cs
+++ View/Input/AI/Logic/MapModel/MapBuilder.cs
@@ -12,10 +12,8 @@ namespace View.Input.AI.Logic.MapModel
     {
         public static MapNode BuildMap(Color[,] board)
         {
-            MapNode head = new MapNode(board.GetAt(0,0));
             //Used as lookup for with node a point belongs to.
             MapNode[,] lookup = new MapNode[board.Height(), board.Width()];
-            lookup[0, 0] = head;
 
             for (int y = 0; y < board.Height(); y++)
             {
@@ -27,22 +25,22 @@ namespace View.Input.AI.Logic.MapModel
 
                     if (isLeftSame && isAboveSame) //check above & to the left
                     {
-                        MapNode left = lookup[x - 1, y];
-                        MapNode above = lookup[x, y - 1];
-                        lookup[x, y] = left;
+                        MapNode left = lookup.GetLeftOf(x, y);
+                        MapNode above = lookup.GetAboveOf(x, y);
+                        lookup.SetAt(x, y, left);
                         left.Merge(above);
                     }
                     else if (isLeftSame) // check to the left
                     {
-                        lookup[x, y] = lookup[x - 1, y];
+                        lookup.SetAt(x, y, lookup.GetLeftOf(x, y));
                     }
                     else if (isAboveSame) // check above
                     {
-                        lookup[x, y] = lookup[x, y - 1];
+                        lookup.SetAt(x, y, lookup.GetAboveOf(x, y));
                     }
                     else
                     {
-                        lookup[x, y] = new MapNode(board.GetAt(x, y));
+                        lookup.SetAt(x, y, new MapNode(board.GetAt(x, y)) );
                     }
 
                     //Add neighbors
@@ -50,27 +48,27 @@ namespace View.Input.AI.Logic.MapModel
                     bool isAboveNotSame = board.CanGetAbove(y) && board.GetAt(x, y) != board.GetAboveOf(x, y);
                     bool isRightNotSame = board.CanGetRight(x) && board.GetAt(x, y) != board.GetRightOf(x, y);
                     bool isBelowNotSame = board.CanGetBelow(y) && board.GetAt(x, y) != board.GetBelowOf(x, y);
-                    MapNode currentNode = lookup[x, y];
+                    MapNode currentNode = lookup.GetAt(x, y);
                     if (isAboveNotSame)
                     {
-                        currentNode.AddNeighbor(lookup[x, y - 1]);
+                        currentNode.AddNeighbor(lookup.GetAboveOf(x, y));
                     }
                     if (isLeftNotSame)
                     {
-                        currentNode.AddNeighbor(lookup[x - 1, y]);
+                        currentNode.AddNeighbor(lookup.GetLeftOf(x, y));
                     }
                     if (isRightNotSame)
                     {
-                        currentNode.AddNeighbor(lookup[x + 1, y]);
+                        currentNode.AddNeighbor(lookup.GetRightOf(x, y));
                     }
                     if (isBelowNotSame)
                     {
-                        currentNode.AddNeighbor(lookup[x, y + 1]);
+                        currentNode.AddNeighbor(lookup.GetBelowOf(x, y));
                     }
                 }
             }
 
-            return head;
+            return lookup.GetAt(0, 0);
         }
     }
 }
